f76c2d9c2050d5cc8aa7edc0509c70453949185d
Jakub Solecki <solko12.98@gmail.com>
2020-03-23 13:11:22 +0100

    Added dependecies (xUnit, JSON serializer, Unirest Api)

---
+++ CalendarBack/Controllers/WeatherController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using unirest_net.http;
+using Newtonsoft.Json;
+
+namespace CalendarBack.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        
+        HttpResponse<string> response = Unirest.get("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=2a0aa79c92d95fff84d4a19951ba6eaf")
+.asJson<string>();
+        
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { response.Body.ToString() };
+        }
+
+        // GET: api/Weather/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return id.ToString();
+        }
+    }
+}
---
+++ CalendarBack/Entities/Weather.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalendarBack.Entities
+{
+    public class Weather
+    {
+        public string temperatureC { get; set; }
+
+    }
+}
