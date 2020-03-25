e943974701ae26cc64e0b330ad7111a4be0eca9b
solko12 <solko12.98@gmail.com>
2020-03-25 14:43:54 +0100

    Added entities and reload endpoint

--- CalendarBack/Controllers/WeatherController.cs
+++ CalendarBack/Controllers/WeatherController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using CalendarBack.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -14,14 +16,26 @@ namespace CalendarBack.Controllers
     [ApiController]
     public class WeatherController : ControllerBase
     {
-        
-        Weather weather = JsonConvert.DeserializeObject<Weather>(Unirest.get("http://api.openweathermap.org/data/2.5/weather?q=Zwoleń&APPID=2a0aa79c92d95fff84d4a19951ba6eaf").asJson<string>().Body.ToString());
+        /// <summary>
+        /// Variable weather store data about current weather
+        /// </summary>
+        static string location = "Zwoleń";
+        Weather weather = JsonConvert.DeserializeObject<Weather>(Unirest.get("http://api.openweathermap.org/data/2.5/weather?q="+ location +"&APPID=2a0aa79c92d95fff84d4a19951ba6eaf").asJson<string>().Body.ToString());
 
-        
+        /// <summary>
+        /// Endpoint for current location
+        /// </summary>
+        /// <returns></returns>
         [HttpGet("location")]
         public IEnumerable<string> Get()
         {
             return new string[] { weather.name };
+        }
+
+        [HttpGet("/")]
+        public string GetWeather() {
+
+            return "";
         }
 
         // GET: api/Weather/5
@@ -30,7 +44,18 @@ namespace CalendarBack.Controllers
         {
             return id.ToString();
         }
-
-
+        
+        // GET: weather/reload
+        [HttpGet("reload")]
+        public Weather reload() {
+            return weather = JsonConvert.DeserializeObject<Weather>(Unirest.get("http://api.openweathermap.org/data/2.5/weather?q=" + location + "&APPID=2a0aa79c92d95fff84d4a19951ba6eaf").asJson<string>().Body.ToString());
+        }
+        /*
+        [HttpGet("TEST")]
+        public string test() {
+            DataSet data = JsonConvert.DeserializeObject<DataSet>(Unirest.get("http://api.openweathermap.org/data/2.5/weather?q=" + location + "&APPID=2a0aa79c92d95fff84d4a19951ba6eaf").asJson<string>().Body.ToString());
+            
+            return data.Tables["cod"].Rows[0].ToString();
+        }*/
     }
 }
