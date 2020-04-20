501053df14f50aab3f35bff4ce4b1eef0e5b4c2e
Jakub Solecki <solko12.98@gmail.com>
2020-04-20 18:44:15 +0200

    Added endpoint /tasks which rreturns List of days with tasks

--- CalendarBack/Controllers/TasksController.cs
+++ CalendarBack/Controllers/TasksController.cs
@@ -56,7 +56,7 @@ namespace CalendarBack.Controllers
         /// <param name="date">Param for specified date</param>
         /// <returns>Json formated shedule object</returns>
         [HttpGet("{date}")]
-        public String GetSheduleForDay(String date)
+        public String GetTasksForDay(String date)
         {
             context.Database.EnsureCreated();
             List<Database.Entities.Shedule> dbShedules = context.Shedules.Where(s => s.Date == DateTime.Parse(date)).ToList();
@@ -83,6 +83,17 @@ namespace CalendarBack.Controllers
                 }
             }
 
+        }
+
+        [HttpGet]
+        public String GetDaysWithTasks() {
+            context.Database.EnsureCreated();
+            List<Database.Entities.Shedule> dbShedules = context.Shedules.Where(s => s.TasksList.Count != 0).ToList();
+            List<DateTime> daysWithTasks = new List<DateTime>();
+            foreach (Database.Entities.Shedule shedule in dbShedules) {
+                daysWithTasks.Add(shedule.Date);
+            }
+            return JsonConvert.SerializeObject(daysWithTasks);
         }
     }
 }
